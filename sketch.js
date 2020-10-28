f9de18b72b7b3801a1991aeac2e7fdb438aa94cc
Aayushi-G <70428294+Aayushi-G@users.noreply.github.com>
2020-10-28 14:09:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground
+var b1,b2,b3
+var ball
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground();
+	rec1=new Box();
+	ball=new Papper();
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+	
+	ball.display()
+	ground.display();
+	rec1.display();
+	
+ 
+  drawSprites();
+ 
+}
+
+
+function keyPressed() {
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:23,y:-40})
+	}
+} 
+
+
+
+
+
+
+
+
+
+
